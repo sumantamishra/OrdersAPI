d1e08a38776fa8c0937ff471787b81211851bedd
Sumant <sumanta.mishra@magicedtech.com>
2020-11-25 13:27:16 +0530

    first commit

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrdersApi.Controllers
+{
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("api/orders")]
+        public IActionResult GetOrders()
+        {
+            
+            var o1 = new Order("ID1", 300);
+            var o2 = new Order("ID2", 300);
+            _logger.LogInformation("-------------message");
+            return Ok(new List<Order> { o1, o2 });
+        }
+
+        [HttpGet]
+        [Route("api/v1/account/list")]
+        public IActionResult GetAccounts()
+        {
+
+            var a1 = new Account("ID1", "Account One");
+            var a2 = new Account("ID2", "Account Two");
+            _logger.LogInformation("-------------message");
+            return Ok(new List<Account> { a1, a2 });
+        }
+    }
+
+
+
+    public class Order
+    {
+        public string Id { get; set; }
+        public decimal Amount { get; set; }
+
+        public Order(string id, decimal amount)
+        {
+            Id = id;
+            Amount = amount;
+        }
+    }
+
+    public class Account
+    {
+        public string Id { get; set; }
+        public string AccountName { get; set; }
+
+        public Account(string id, string account)
+        {
+            Id = id;
+            AccountName = account;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Compact;
+//using Serilog.Formattin
+
+
+namespace OrdersApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            /*Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();*/
+
+
+            //CreateHostBuilder(args).Build().Run();
+
+            Log.Logger = new LoggerConfiguration()
+             .MinimumLevel.Debug()
+             .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+             .Enrich.FromLogContext()
+             .WriteTo.Seq("http://10.11.8.210:5341")//.Console(new RenderedCompactJsonFormatter())
+             .CreateLogger();
+
+            try
+            {
+                Log.Information("----------Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    
+                });
+    }
+}
